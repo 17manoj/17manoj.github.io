741a47b3cfe16306de81e412044d03e344806679
Nrawat6 <nidhirawat1993.nr@gmail.com>
2026-06-07 16:00:14 +0100

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  base: '/manoj-portfolio-app/', // Adjust this base path for GitHub Pages
+  // Set the base path for GitHub Pages deployment at the root
+  base: '/',
   build: {
     outDir: 'dist',
-    sourcemap: true,
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild'
+  }
 });
